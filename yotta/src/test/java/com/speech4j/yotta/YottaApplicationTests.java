99c7193e85a268f65d2c83e481de9e509407d791
Dima Pavlov <misterscors@gmail.com>
2020-02-09 22:50:56 +0200

    GCS and YoTTA moduels initialized, controller and basic logic added to GCS

---
+++ yotta/src/test/java/com/speech4j/yotta/YottaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speech4j.yotta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YottaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
