5c9f4685df7550ae259e4a44f5e40cda9523a969
“Dmytro <Dmytro.Pavlov@davita.com>
2020-03-11 19:25:32 +0200

    Fix endpoints

--- sttc-apigw/src/main/java/com/speech4j/apigw/control/stt/WitSpeechRecognitionController.java
+++ sttc-apigw/src/main/java/com/speech4j/apigw/control/stt/WitSpeechRecognitionController.java
@@ -5,11 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController("/wit")
+@RestController
+@RequestMapping("/wit")
 public class WitSpeechRecognitionController {
 
     @GetMapping("/youtube")
